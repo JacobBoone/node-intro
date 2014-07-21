c0a047d40f7983bfbcc2436c28431f1556e817b7
Jacob <jakeboone@aol.com>
2014-07-21 12:52:39 -0600

    added bonus for adding multiple files into one file

--- challenge.js
+++ challenge.js
@@ -1,11 +1,52 @@
 
 
-// copy a file to a new file
-var fs = require('fs')
+// copy contents of specific file to a new specific file
+// var fs = require('fs')
 
-var copy = fs.readFileSync('1.txt', 'utf-8')
+// var copy = fs.readFileSync('1.txt', 'utf-8')
 
- fs.writeFileSync('2.txt', copy)
+//  fs.writeFileSync('2.txt', copy)
 
 
-console.log(copy)
+// console.log(copy)
+
+
+
+
+// copy a file to a new file in a generic way
+
+
+// var fs = require('fs')
+
+// var copy = fs.readFileSync(process.argv[2], 'utf-8')
+
+//  fs.writeFileSync(process.argv[3], copy)
+
+
+// console.log(copy)
+
+
+
+
+// copy many filea into a 1 new file in a generic way
+
+
+
+var fs = require('fs');
+
+
+
+var output = '' ;
+	for (var i = 2; i < process.argv.length; i++) {
+		var newFile = fs.readFileSync(process.argv[i], 'utf-8');
+
+		console.log(newFile);
+		output = output + newFile;
+	};
+
+ 
+
+
+ fs.writeFileSync('3.txt', output)
+
+
